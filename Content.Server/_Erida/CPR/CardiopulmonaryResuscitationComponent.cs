0ebe101bd5b2a1019d59099e2481beb52c163d60
vanomorodellefake <vladimirkrasutskiy2@mail.ru>
2026-01-28 23:55:37 +0500

    Final fun things

--- Content.Server/_Erida/CPR/CardiopulmonaryResuscitationComponent.cs
+++ Content.Server/_Erida/CPR/CardiopulmonaryResuscitationComponent.cs
@@ -3,5 +3,9 @@ namespace Content.Shared.Interaction.Components;
 [RegisterComponent, Access(typeof(CardiopulmonaryResuscitationSystem))]
 public sealed partial class CardiopulmonaryResuscitationComponent : Component
 {
+    [DataField]
+    public float CRPAsphyxiationAmount = -3f;
 
+    [DataField]
+    public int CRPHowLong = 3;
 }
--- Content.Server/_Erida/CPR/CardiopulmonaryResuscitationSystem.cs
+++ Content.Server/_Erida/CPR/CardiopulmonaryResuscitationSystem.cs
@@ -1,34 +1,34 @@
-using System.Runtime.CompilerServices;
-using System.Threading;
 using Content.Server.DoAfter;
 using Content.Shared.CRP;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Components;
 using Content.Shared.DoAfter;
 using Content.Shared.FixedPoint;
+using Content.Shared.IdentityManagement;
 using Content.Shared.Interaction.Components;
-using Content.Shared.Interaction.Events;
-using Content.Shared.Mobs.Components;
 using Content.Shared.Mobs.Systems;
+using Content.Shared.Nutrition.EntitySystems;
+using Content.Shared.Popups;
 using Content.Shared.Verbs;
-using Robust.Shared.Serialization;
+using Robust.Shared.Player;
+using Robust.Shared.Profiling;
 using DependencyAttribute = Robust.Shared.IoC.DependencyAttribute;
 
 namespace Content.Shared.Interaction;
 
 public sealed class CardiopulmonaryResuscitationSystem : EntitySystem
 {
-    [Dependency] protected readonly EntityManager EntityManager = default!;
-    [Dependency] private readonly InteractionPopupSystem _interactionPopupSystem = default!;
+    [Dependency] protected readonly EntityManager _entityManager = default!;
     [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
     [Dependency] private readonly DoAfterSystem _doAfterSystem = default!;
     [Dependency] private readonly Damage.Systems.DamageableSystem _damageable = default!;
+    [Dependency] private readonly IngestionSystem _ingestionSystem = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<CardiopulmonaryResuscitationComponent, GetVerbsEvent<AlternativeVerb>>(AddCRPVerb);
-        SubscribeLocalEvent<CardiopulmonaryResuscitationComponent, InteractHandEvent>(OnInteractHand); // InteractUsingEvent InteractHandEvent
         SubscribeLocalEvent<CardiopulmonaryResuscitationComponent, CRPDoAfterEvent>(CRPDoAfter);
     }
 
@@ -52,70 +52,98 @@ public sealed class CardiopulmonaryResuscitationSystem : EntitySystem
                 StartCRP(args.User, uid, component);
             },
             Text = Loc.GetString("CRP-verb"),
-            Priority = 2
+            Priority = 3
         };
 
         args.Verbs.Add(verb);
     }
 
-    public void OnInteractHand(EntityUid uid, CardiopulmonaryResuscitationComponent component, InteractHandEvent args)
+    private void StartCRP(EntityUid сRPer, EntityUid сRPied, CardiopulmonaryResuscitationComponent component)
     {
-        if (args.Handled)
+        if (!CheckMouth(сRPer, сRPied))
             return;
 
-        if (!(TryComp<MobStateComponent>(args.Target, out var mobState)
-            && _mobStateSystem.IsCritical(args.Target, mobState)))
-        {
-            return;
-        }
+        SendPopupMessages(сRPer, сRPied);
 
-        StartCRP(uid, args.Target, component);
-    }
-
-    private void StartCRP(EntityUid CRPer, EntityUid CRPied, CardiopulmonaryResuscitationComponent component)
-    {
-        TimeSpan length = TimeSpan.FromSeconds(3);
-
-        //component.CancelToken = new CancellationTokenSource();
+        TimeSpan length = TimeSpan.FromSeconds(component.CRPHowLong);
 
         var ev = new CRPDoAfterEvent();
-        var args = new DoAfterArgs(EntityManager, CRPer, length, ev, CRPied, target: CRPied)
+        var args = new DoAfterArgs(_entityManager, сRPer, length, ev, сRPied, target: сRPied)
         {
             BreakOnMove = true,
             BreakOnDamage = true,
-            //NeedHand = true
         };
 
         _doAfterSystem.TryStartDoAfter(args);
     }
     private void CRPDoAfter(EntityUid uid, CardiopulmonaryResuscitationComponent component, CRPDoAfterEvent args)
     {
-        //component.CancelToken = null;
         if (args.Handled || args.Cancelled)
             return;
 
-        CRP(args.Args.User, uid);
+        if (!CheckMouth(args.Args.User, uid))
+            return;
+
+        CRP(uid, component.CRPAsphyxiationAmount);
         args.Repeat = true;
         args.Args.Event.Repeat = args.Repeat;
         args.Handled = true;
     }
 
-    private void CRP(EntityUid CRPer, EntityUid CRPied)
+    private void CRP(EntityUid сRPied, float сRPAsphyxiationAmount)
     {
-        if (!TryComp<DamageableComponent>(CRPied, out var DCcomp))
+        if (!TryComp<DamageableComponent>(сRPied, out var dCcomp))
             return;
 
         var damageSpec = new DamageSpecifier();
         damageSpec.DamageDict = new Dictionary<string, FixedPoint2>()
         {
-            { "Asphyxiation", FixedPoint2.New(-5) }
+            { "Asphyxiation", FixedPoint2.New(сRPAsphyxiationAmount) }
         };
 
         _damageable.TryChangeDamage(
-            (CRPied, DCcomp),
+            (сRPied, dCcomp),
             damageSpec,
             ignoreResistances: true,
             interruptsDoAfters: true
         );
     }
+
+    private bool CheckMouth(EntityUid cRPer, EntityUid cRPied)
+    {
+        if (!_ingestionSystem.HasMouthAvailable(cRPer, cRPer))
+        {
+            _popup.PopupEntity(Loc.GetString("CRP-CRPer-has-blocked-mouth"), cRPer, cRPer);
+            return false;
+        }
+
+        if (!_ingestionSystem.HasMouthAvailable(cRPied, cRPied))
+        {
+            _popup.PopupEntity(Loc.GetString("CRP-CRPied-has-blocked-mouth"), cRPer, cRPer);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void SendPopupMessages(EntityUid cRPer, EntityUid cRPied)
+    {
+        _popup.PopupEntity(Loc.GetString("CRP-start-CRP-CRPer",
+            ("CRPied", Identity.Name(cRPied, EntityManager, cRPer))),
+            cRPied, cRPer, Shared.Popups.PopupType.Medium);
+        _popup.PopupEntity(Loc.GetString("CRP-start-CRP-CRpied",
+            ("CRPer", Identity.Name(cRPer, EntityManager, cRPied))),
+            cRPer, cRPied, Shared.Popups.PopupType.Medium);
+
+        var filter = Filter.Pvs(cRPer, entityManager: EntityManager);
+        if (filter != null)
+        {
+            _popup.PopupEntity(Loc.GetString("CRP-start-CRP-others",
+                ("CRPer", Identity.Name(cRPer, EntityManager)),
+                ("CRPied", Identity.Name(cRPied, EntityManager))),
+                cRPer, filter.RemoveWhere(e => e.AttachedEntity == cRPer || e.AttachedEntity == cRPied),
+                true, Shared.Popups.PopupType.Medium);
+        }
+    }
 }
+
